7c54a5cdd1ddcf1744df3d2b486cccd925f4338e
Minniti Julien <minnitijulien06@gmail.com>
2024-02-20 16:28:26 +0100

    (v0.61a) Minor Fix

--- transfectionratioxpert.py
+++ transfectionratioxpert.py
@@ -1,14 +1,8 @@
-import json
-import os
-from datetime import datetime
+import datetime
 import io
 
-import altair as alt
-import numpy as np
 import pandas as pd
 import streamlit as st
-from scipy.optimize import minimize
-
 
 culture_vessel_options_lipo = {"96-well": 5, "24-well": 25, "12-well": 50, "6-well/35-mm": 125,
                                "60-mm/flask 25 cm2": 250, "10-cm/flask 75 cm2": 500, "15-cm/flask 175cm2": 750}
@@ -36,14 +30,16 @@ st.sidebar.write("**List and condition(s)**")
 df = st.sidebar.data_editor(df, key="vector", num_rows="dynamic")
 vector_table = df.to_dict(orient='records')
 
-num_columns = st.sidebar.number_input('Number of condition(s)', min_value=1, max_value=None, value=1, step=1, help=None, key="mix")
+num_columns = st.sidebar.number_input('Number of condition(s)', min_value=1, max_value=None, value=1, step=1, help=None,
+                                      key="mix")
 
 st.sidebar.divider()
 st.sidebar.write("**Settings for all conditions**")
 vector_for_all_mix = st.sidebar.toggle("Vector(s) per condition")
 if vector_for_all_mix:
-    vector_per_mix = st.sidebar.number_input('Vector per condition', min_value=1, max_value=len(df), value=1, step=1,help=None,
-                                                 key=f"vector_per_mix")
+    vector_per_mix = st.sidebar.number_input('Vector per condition', min_value=1, max_value=len(df), value=1, step=1,
+                                             help=None,
+                                             key=f"vector_per_mix")
 
 amount_dna_for_all = st.sidebar.toggle("Amount of DNA per well")
 if amount_dna_for_all:
@@ -69,7 +65,8 @@ if vector_for_all_mix:
                         f"not available. The previous plasmid(s)/vector(s)/RNA already uses all the DNA required for transfection.")
                     amount_dna_selected = 0
             else:
-                st.sidebar.warning("Amount of vector available only if you select the option to define the amount of DNA desired")
+                st.sidebar.warning(
+                    "Amount of vector available only if you select the option to define the amount of DNA desired")
             calcul_j.append([j, vector_selected, amount_dna_selected if amount_dna_for_all else ""])
 
 transfection_type_for_all = st.sidebar.toggle("Transfection type")
@@ -86,13 +83,14 @@ else:
 dna_ratio_for_all = st.sidebar.toggle("DNA ratio")
 if dna_ratio_for_all:
     dna_ratio = st.sidebar.number_input("DNA ratio", min_value=0.1, max_value=None, value=1.0, step=0.1, help=None,
-                                key=f"dna_ratio")
+                                        key=f"dna_ratio")
     reagent_ratio = st.sidebar.number_input("Reagent ratio", min_value=0.1, max_value=None, value=2.0, step=0.1,
-                                                help=None, key=f"reagent_ratio")
+                                            help=None, key=f"reagent_ratio")
 
 culture_vessel_for_all = st.sidebar.toggle('Culture Vessel')
 if culture_vessel_for_all:
-    culture_vessel = st.sidebar.selectbox("Culture Vessel", list(culture_vessel_options_lipo.keys()), key=f"culture_vessel")
+    culture_vessel = st.sidebar.selectbox("Culture Vessel", list(culture_vessel_options_lipo.keys()),
+                                          key=f"culture_vessel")
 
 numbers_wells_for_all = st.sidebar.toggle("Number of well(s)")
 if numbers_wells_for_all:
@@ -120,23 +118,29 @@ try:
                                             help=None, key=f"reagent_ratio{i}")
             ratio = dna_ratio / reagent_ratio
             culture_vessel = st.selectbox("Culture Vessel",
-                                          list(culture_vessel_options_lipo.keys()) if culture_vessel_for_all is False else [culture_vessel],
+                                          list(
+                                              culture_vessel_options_lipo.keys()) if culture_vessel_for_all is False else [
+                                              culture_vessel],
                                           key=f"culture_vessel{i}")
             number_wells = st.number_input("Number of well(s)", min_value=1, max_value=None,
                                            value=1 if numbers_wells_for_all is False else number_wells, step=1,
                                            help=None, key=f"number_well{i}")
-            culture_vessel_value = culture_vessel_options_lipo[culture_vessel] if transfection_type == "Lipofectamine (2000/3000)"\
+            culture_vessel_value = culture_vessel_options_lipo[
+                culture_vessel] if transfection_type == "Lipofectamine (2000/3000)" \
                 else culture_vessel_options_jetprime[culture_vessel]
             for j in range(1, vector_per_mix + 1):
                 vector_selected = st.selectbox(f'Vector {j}',
-                                               df["Plasmid/Vector/RNA"] if selected_vector is False else [st.session_state[f"vector_selected{j}"]],
+                                               df["Plasmid/Vector/RNA"] if selected_vector is False else [
+                                                   st.session_state[f"vector_selected{j}"]],
                                                key=f'vector_selected{i}-{j}')
                 sum_amount_dna_selected_same_i = sum(amount_dna_selected for item in calcul if item[0] == i)
                 max_slider = amount_of_dna - sum_amount_dna_selected_same_i
                 if max_slider > 0:
                     amount_dna_selected = st.slider(
                         f'Amount of {vector_selected if vector_selected is not None or vector_selected != "" else f"Vector {j}"} (µg)',
-                        0.0, max_slider, value=0.0 if selected_vector is False or amount_dna_for_all is False else st.session_state[f"amount_vector_selected{j}"],
+                        0.0, max_slider,
+                        value=0.0 if selected_vector is False or amount_dna_for_all is False else st.session_state[
+                            f"amount_vector_selected{j}"],
                         step=0.1, key=f'amount_vector_selected{i}-{j}')
                 else:
                     st.warning(
@@ -152,31 +156,36 @@ try:
             results_dict[vector_selected] = {
                 "Plasmid/Vector/RNA": vector_selected,
                 f"Condition {i} (µL)": (amount_dna_selected / next(item['µg/µL'] for item in vector_table
-                                                             if item["Plasmid/Vector/RNA"] == vector_selected)) * number_wells
+                                                                   if item[
+                                                                       "Plasmid/Vector/RNA"] == vector_selected)) * number_wells
             }
         else:
-            results_dict[vector_selected][f"Condition {i} (µL)"] = (amount_dna_selected / next(item['µg/µL'] for item in vector_table
-                                                                                         if item["Plasmid/Vector/RNA"] == vector_selected)) * number_wells
+            results_dict[vector_selected][f"Condition {i} (µL)"] = (amount_dna_selected / next(
+                item['µg/µL'] for item in vector_table
+                if item["Plasmid/Vector/RNA"] == vector_selected)) * number_wells
 
         if transfection_type not in results_dict:
             results_dict[transfection_type] = {
-                        "Plasmid/Vector/RNA": transfection_type, f"Condition {i} (µL)": (amount_of_dna / ratio) * number_wells}
+                "Plasmid/Vector/RNA": transfection_type, f"Condition {i} (µL)": (amount_of_dna / ratio) * number_wells}
         else:
             results_dict[transfection_type][f"Condition {i} (µL)"] = (amount_of_dna / ratio) * number_wells
 
         if transfection_type == "Lipofectamine (2000/3000)":
             if "Culture Vessel Lipo 1" not in results_dict:
                 results_dict["Culture Vessel Lipo 1"] = {
-                    "Plasmid/Vector/RNA": "OptiMEM w/ Plasmid/Vector/RNA", f"Condition {i} (µL)": culture_vessel_value * number_wells}
+                    "Plasmid/Vector/RNA": "OptiMEM w/ Plasmid/Vector/RNA",
+                    f"Condition {i} (µL)": culture_vessel_value * number_wells}
                 results_dict["Culture Vessel Lipo 2"] = {
-                    "Plasmid/Vector/RNA": "OptiMEM w/ Lipofectamine", f"Condition {i} (µL)": culture_vessel_value * number_wells}
+                    "Plasmid/Vector/RNA": "OptiMEM w/ Lipofectamine",
+                    f"Condition {i} (µL)": culture_vessel_value * number_wells}
             else:
                 results_dict["Culture Vessel Lipo 1"][f"Condition {i} (µL)"] = culture_vessel_value * number_wells
                 results_dict["Culture Vessel Lipo 2"][f"Condition {i} (µL)"] = culture_vessel_value * number_wells
 
         elif transfection_type == "jetPRIME":
             if "Culture Vessel JP" not in results_dict:
-                results_dict["Culture Vessel JP"] = {"Plasmid/Vector/RNA": "jetPRIME Buffer", f"Condition {i} (µL)": culture_vessel_value * number_wells}
+                results_dict["Culture Vessel JP"] = {"Plasmid/Vector/RNA": "jetPRIME Buffer",
+                                                     f"Condition {i} (µL)": culture_vessel_value * number_wells}
             else:
                 results_dict["Culture Vessel JP"][f"Condition {i} (µL)"] = culture_vessel_value * number_wells
 
@@ -216,5 +225,6 @@ try:
                        mime="application/vnd.ms-excel", key='download-excel')
 
 except Exception as e:
-    st.write("Add the plasmids/vectors/RNA in the left panel :) A small arrow at the top left of the screen will allow you to open it if this is not the case :)")
+    st.write(
+        "Add the plasmids/vectors/RNA in the left panel :) A small arrow at the top left of the screen will allow you to open it if this is not the case :)")
     st.write(e)
